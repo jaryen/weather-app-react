30bb92d51b09197a5dea9bdf6bda7eba0ab3f3ab
Jared Yen <jaryen@uw.edu>
2022-09-24 11:47:55 -0400

    Updated to add autocomplete place search. Need to render outputs

--- src/SearchCity.js
+++ src/SearchCity.js
@@ -1,8 +1,7 @@
-import React from "react";
+import React, {useState} from "react";
 import Input from '@mui/material/Input';
 import Button from '@mui/material/Button';
 import SearchIcon from '@mui/icons-material/Search';
-// import Popover from '@mui/material/Popover';
 import UilSnowflake from '@iconscout/react-unicons/icons/uil-snowflake';
 import UilSun from '@iconscout/react-unicons/icons/uil-sun';
 import UilRain from '@iconscout/react-unicons/icons/uil-raindrops';
@@ -12,8 +11,8 @@ import UilWarm from '@iconscout/react-unicons/icons/uil-cloud-sun';
 // Open Weather API Key
 const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
 
-// Mapbox API Key
-const MAPBOX_API_KEY = process.env.REACT_MAPBOX_API_KEY;
+// Geoapify API Key
+const GEOAPIFY_AUTOCOMPLETE_API_KEY = process.env.REACT_APP_GEOAPIFY_API_KEY;
 
 // Data for holding forecast info.
 const cnt = 40;
@@ -23,13 +22,25 @@ const tempCard = {
     high_temp: null,
     low_temp: null,
     icon: null,
-}
+};
 var tempCards = [];
 const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+const address = {
+    city_name: null,
+    state_code: null,
+    country_code: null,
+    address: null
+};
+// const [places, updatePlaces] = useState('');
 
 class SearchCity extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            places: []
+        }
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -61,6 +72,36 @@ class SearchCity extends React.Component {
         }
     }
 
+    async addressAutocomplete(searchInput) {
+        fetch(`https://api.geoapify.com/v1/geocode/autocomplete?text=${searchInput}&type=city&format=json&apiKey=${GEOAPIFY_AUTOCOMPLETE_API_KEY}`)
+            .then((res) => {
+                if (res.ok) {
+                    return res.json();
+                } else {
+                    throw new Error("Geoapify API call has invalid input.");
+                }
+            })
+            .then((data) => {
+                console.log(data);
+                let results = data.results;
+                console.log(results);
+                let addresses = [];
+                for (let i = 0; i < results.length; i++) {
+                    let currAddress = Object.create(address);
+                    currAddress.city_name = results[i].city;
+                    currAddress.state_code = results[i].state_code;
+                    currAddress.country_code = results[i].country_code;
+                    currAddress.address = results[i].formatted;
+                    addresses.push(currAddress);
+                }
+                this.setState({places: addresses});
+                console.log(this.state.places);
+            })
+            .catch((err) => {
+                console.error('Error calling Geoapify API: ', err);
+            });
+    }
+    
     // Search City API Call
     async searchCity(city_name) {
         console.log("City Searched: " + city_name);
@@ -111,6 +152,7 @@ class SearchCity extends React.Component {
 
     handleChange(event) {
         this.props.onCitySearchChange(event.target.value);
+        this.addressAutocomplete(event.target.value);
     }
 
     handleSubmit(event) {
@@ -125,6 +167,8 @@ class SearchCity extends React.Component {
                 <br/>
                 <br/>
                 <Button type="submit" variant='contained' endIcon={<SearchIcon />}>Search</Button>
+                <br/>
+                <div class="autocomplete-container" id="autocomplete-container" ref={this.myRef}></div>
             </form>
         );
     }
