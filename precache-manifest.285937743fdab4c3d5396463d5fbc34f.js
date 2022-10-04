db8e682c803aabc2038252991fe99b9003349085
Jared Yen <jaryen@uw.edu>
2022-10-04 15:34:34 -0400

    Deploy react app to github pages.

---
+++ precache-manifest.285937743fdab4c3d5396463d5fbc34f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9201189c1814d6c86472",
+    "url": "/react-gh-pages/static/css/main.ff676cd6.chunk.css"
+  },
+  {
+    "revision": "9201189c1814d6c86472",
+    "url": "/react-gh-pages/static/js/main.9201189c.chunk.js"
+  },
+  {
+    "revision": "611e4dfc9e3c7cc57587",
+    "url": "/react-gh-pages/static/js/1.611e4dfc.chunk.js"
+  },
+  {
+    "revision": "659bb1672f8da44b7ebe",
+    "url": "/react-gh-pages/static/js/2.659bb167.chunk.js"
+  },
+  {
+    "revision": "5e03d8874b3908826758",
+    "url": "/react-gh-pages/static/js/runtime~main.5e03d887.js"
+  },
+  {
+    "revision": "a68818f5c0d5c02431f9856adc2697b5",
+    "url": "/react-gh-pages/index.html"
+  }
+];
